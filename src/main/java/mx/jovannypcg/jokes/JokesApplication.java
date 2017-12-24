7bd2a05ba9caa4d784b5767cd37ef923dc5ab46e
Jovanny Cruz <jovannypcg@yahoo.com>
2017-12-24 17:56:22 -0600

    First commit

---
+++ src/main/java/mx/jovannypcg/jokes/JokesApplication.java
@@ -0,0 +1,11 @@
+package mx.jovannypcg.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JokesApplication.class, args);
+	}
+}
