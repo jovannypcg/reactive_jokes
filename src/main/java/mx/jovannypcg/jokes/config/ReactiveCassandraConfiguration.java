31da19e0e45e1d1dffbe8c3f75c3d0ca48cf2fe9
Jovanny Cruz <jovannypcg@yahoo.com>
2017-12-24 18:13:08 -0600

    Add cassandra configuration to create keyspaces

---
+++ src/main/java/mx/jovannypcg/jokes/config/ReactiveCassandraConfiguration.java
@@ -0,0 +1,45 @@
+package mx.jovannypcg.jokes.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractReactiveCassandraConfiguration;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.DataCenterReplication;
+import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
+import org.springframework.data.cassandra.repository.config.EnableReactiveCassandraRepositories;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableReactiveCassandraRepositories
+public class ReactiveCassandraConfiguration extends AbstractReactiveCassandraConfiguration {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReactiveCassandraConfiguration.class);
+
+    @Value("${spring.data.cassandra.keyspace-name}")
+    private String keyspaceName;
+
+    @Override
+    protected String getKeyspaceName() {
+        LOGGER.info("Getting into [{}] keyspace", keyspaceName);
+        return keyspaceName;
+    }
+
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        return Collections.singletonList(getCreateKeyspaceSpecification());
+    }
+
+    private CreateKeyspaceSpecification getCreateKeyspaceSpecification() {
+        DataCenterReplication dcr = DataCenterReplication.of("dcr", 3L);
+
+        LOGGER.info("Creating [{}] keyspace if it does not exist", keyspaceName);
+        return CreateKeyspaceSpecification
+                .createKeyspace(keyspaceName)
+                .ifNotExists()
+                .with(KeyspaceOption.DURABLE_WRITES, true)
+                .withNetworkReplication(dcr);
+    }
+}
