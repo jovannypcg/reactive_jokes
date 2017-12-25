d8d65f89e417dd68c24b5e75c7d72db17fab281f
Jovanny Cruz <jovannypcg@yahoo.com>
2017-12-24 20:51:18 -0600

    Add missing ServerRequest argument to findAll method

--- src/main/java/mx/jovannypcg/jokes/web/JokeHandler.java
+++ src/main/java/mx/jovannypcg/jokes/web/JokeHandler.java
@@ -3,12 +3,13 @@ package mx.jovannypcg.jokes.web;
 import mx.jovannypcg.jokes.domain.Joke;
 import mx.jovannypcg.jokes.service.JokeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Service
+@Component
 public class JokeHandler {
     private JokeService jokeService;
 
@@ -17,7 +18,7 @@ public class JokeHandler {
         this.jokeService = jokeService;
     }
 
-    public Mono<ServerResponse> findAll() {
+    public Mono<ServerResponse> findAll(ServerRequest request) {
         Flux<Joke> jokes = jokeService.findAll();
         return  ServerResponse.ok().body(jokes, Joke.class);
     }
